68bfb835e09beadc8367456c7a6ef44fd4adcbcc
SAHIL KUMAR <119514315+sahilkumarclass@users.noreply.github.com>
2024-11-26 11:37:20 +0530

    Update Contact.js

--- src/page/Contact/Contact.js
+++ src/page/Contact/Contact.js
@@ -24,7 +24,7 @@ const Contact = () => {
         setLoading(true);
 
         try {
-            const res = await axios.post("https://portfolio-website-backend-production-d5be.up.railway.app/api/v1/portfolio/sendEmail", {
+            const res = await axios.post("portfolio-website-backend-production-0f84.up.railway.app", {
                 name,
                 email,
                 msg,
